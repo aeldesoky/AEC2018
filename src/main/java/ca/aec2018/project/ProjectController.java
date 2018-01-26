31affc741ac76735078600d6fc051c7fa45294a6
Jacob Smith <jacob.smith@unb.ca>
2018-01-26 11:21:43 -0400

    added dropdown

--- src/main/java/ca/aec2018/project/ProjectController.java
+++ src/main/java/ca/aec2018/project/ProjectController.java
@@ -1,5 +1,7 @@
 package ca.aec2018.project;
 
+import ca.aec2018.project.model.WindRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @EnableAutoConfiguration
 @Controller
 public class ProjectController {
+    @Autowired
+    WindRepository windRepository;
+
     @GetMapping("/")
     public String index() {
         return "index";
