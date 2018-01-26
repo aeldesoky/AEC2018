ba2a496c6eaab9dc5c7a2dd693effd3c8fb02d35
Brandon Richardson <brandon1024.br@gmail.com>
2018-01-26 13:39:12 -0330

    changing entities

--- src/main/java/ca/aec2018/project/model/Wind.java
+++ src/main/java/ca/aec2018/project/model/Wind.java
@@ -12,16 +12,120 @@ public class Wind implements Renewable {
     @OneToOne
     private Coordinate coordinate;
 
-    @ElementCollection
-    private List<Double> months;
+    private Double january;
+    private Double february;
+    private Double march;
+    private Double april;
+    private Double may;
+    private Double june;
+    private Double july;
+    private Double august;
+    private Double september;
+    private Double october;
+    private Double novermber;
+    private Double december;
+
+    public void setCoordinate(Coordinate coordinate) {
+        this.coordinate = coordinate;
+    }
+
+    public Double getJanuary() {
+        return january;
+    }
+
+    public void setJanuary(Double january) {
+        this.january = january;
+    }
+
+    public Double getFebruary() {
+        return february;
+    }
+
+    public void setFebruary(Double february) {
+        this.february = february;
+    }
+
+    public Double getMarch() {
+        return march;
+    }
+
+    public void setMarch(Double march) {
+        this.march = march;
+    }
+
+    public Double getApril() {
+        return april;
+    }
+
+    public void setApril(Double april) {
+        this.april = april;
+    }
+
+    public Double getMay() {
+        return may;
+    }
+
+    public void setMay(Double may) {
+        this.may = may;
+    }
+
+    public Double getJune() {
+        return june;
+    }
+
+    public void setJune(Double june) {
+        this.june = june;
+    }
+
+    public Double getJuly() {
+        return july;
+    }
+
+    public void setJuly(Double july) {
+        this.july = july;
+    }
+
+    public Double getAugust() {
+        return august;
+    }
+
+    public void setAugust(Double august) {
+        this.august = august;
+    }
+
+    public Double getSeptember() {
+        return september;
+    }
+
+    public void setSeptember(Double september) {
+        this.september = september;
+    }
+
+    public Double getOctober() {
+        return october;
+    }
+
+    public void setOctober(Double october) {
+        this.october = october;
+    }
+
+    public Double getNovermber() {
+        return novermber;
+    }
+
+    public void setNovermber(Double novermber) {
+        this.novermber = novermber;
+    }
+
+    public Double getDecember() {
+        return december;
+    }
+
+    public void setDecember(Double december) {
+        this.december = december;
+    }
 
     private Double annualAverage;
-
-    public Wind(Coordinate coordinate, List<Double> months, Double annualAverage) {
-        this.coordinate = coordinate;
-        this.months = months;
-        this.annualAverage = annualAverage;
-    }
 
     public Integer getId() {
         return id;
@@ -29,15 +133,6 @@ public class Wind implements Renewable {
 
     public void setId(Integer id) {
         this.id = id;
-    }
-
-    @Override
-    public List<Double> getMonths() {
-        return months;
-    }
-
-    public void setMonths(List<Double> months) {
-        this.months = months;
     }
 
     @Override
