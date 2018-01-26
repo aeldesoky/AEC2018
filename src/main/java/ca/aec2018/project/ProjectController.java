137bd63dd7cd80360a359f4dd0d0fc40983f419e
Jacob Smith <jacob.smith@unb.ca>
2018-01-26 09:42:28 -0400

    added exceptions, dependencies, controller

---
+++ src/main/java/ca/aec2018/project/ProjectController.java
@@ -0,0 +1,11 @@
+package ca.aec2018.project;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+
+
+
+@EnableAutoConfiguration
+@Controller
+public class ProjectController {
+}
