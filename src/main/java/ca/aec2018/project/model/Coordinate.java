c447d7a4e24f29871cd00dcd293f2d6e044d58c4
alexodonn <alex.odonnell@unb.ca>
2018-01-26 10:03:30 -0400

    updated model with getters and setters

--- src/main/java/ca/aec2018/project/model/Coordinate.java
+++ src/main/java/ca/aec2018/project/model/Coordinate.java
@@ -10,4 +10,32 @@ public class Coordinate {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    private Integer latitude;
+
+    private Integer longitude;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Integer latitude) {
+        this.latitude = latitude;
+    }
+
+    public Integer getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Integer longitude) {
+        this.longitude = longitude;
+    }
 }
--- src/main/java/ca/aec2018/project/model/Wind.java
+++ src/main/java/ca/aec2018/project/model/Wind.java
@@ -10,4 +10,32 @@ public class Wind {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    private Double[] months;
+
+    private Double annualAverage;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double[] getMonths() {
+        return months;
+    }
+
+    public void setMonths(Double[] months) {
+        this.months = months;
+    }
+
+    public Double getAnnualAverage() {
+        return annualAverage;
+    }
+
+    public void setAnnualAverage(Double annualAverage) {
+        this.annualAverage = annualAverage;
+    }
 }
