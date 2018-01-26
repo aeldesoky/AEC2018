6f2455c2659826aa8b278ae623b01ebba93c316b
Jacob Smith <jacob.smith@unb.ca>
2018-01-26 09:54:44 -0400

    added dependencies for mysql

--- src/main/java/ca/aec2018/project/ProjectController.java
+++ src/main/java/ca/aec2018/project/ProjectController.java
@@ -2,10 +2,14 @@ package ca.aec2018.project;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
-
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @EnableAutoConfiguration
 @Controller
 public class ProjectController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
