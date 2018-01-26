6fab76efdc27629275f634c752501569701c97ca
Jacob Smith <jacob.smith@unb.ca>
2018-01-26 10:07:16 -0400

    fixed jtwig

---
+++ src/main/java/ca/aec2018/project/UrlHandlerMappingConfig.java
@@ -0,0 +1,15 @@
+package ca.aec2018.project;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class UrlHandlerMappingConfig extends WebMvcConfigurationSupport {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+}
